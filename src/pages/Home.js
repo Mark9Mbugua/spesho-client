b777b36afc4d87e5fa28a2dad7c281d77fa84a11
Markize <mbuguamark@gmail.com>
2020-02-14 16:04:22 +0300

    created footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Items from '../components/items/Items';
+import Footer from '../components/common/Footer';
 
 export default function Home() {
     return (
         <div>
             <Items />
+            <Footer />
         </div>
     )
 }
